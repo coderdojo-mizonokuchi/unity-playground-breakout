5972ff222e1b69ceae5660a685912b84774e12b7
Hisashi Komori <hkomo746@gmail.com>
2019-05-03 13:57:31 +0900

    ブロックの配置。

--- Project/Assets/Scripts/PlayerScript.cs
+++ Project/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,8 @@ public class PlayerScript : MonoBehaviour
     private Vector2 initialPosition = new Vector2(0f, -7f);
     private Vector2 initialForce = new Vector2(50f, 50f);
     private Rigidbody2D rigidbody = null;
+    private float velocityMin = 100f;
+    private float velocityNow = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,16 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigidbody.velocity *= 1.0001f;
+        if (Mathf.Abs(rigidbody.velocity.x) > 0f && Mathf.Abs(rigidbody.velocity.y / rigidbody.velocity.x) < 0.1f)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y * 1.01f);
+        }
+        if (Mathf.Abs(rigidbody.velocity.y) > 0f && Mathf.Abs(rigidbody.velocity.x / rigidbody.velocity.y) < 0.1f)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x * 1.01f, rigidbody.velocity.y);
+        }
+        velocityNow *= 1.0001f;
+        rigidbody.velocity *= velocityNow / rigidbody.velocity.SqrMagnitude();
     }
 
     public void ResetPlayer()
